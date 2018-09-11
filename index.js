aec520cc51afd51449591f7ff8ae144e86de07a4
mars <mars@mars.com>
2018-09-11 18:32:03 +0800

    merge NODE_ENV to root

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const path = require('path')
 class CoilsEnv {
 	static mounted (application, envPath) {
-		envPath = envPath || path.resolve(process.cwd(), 'config/env.js')
-		let env = require(envPath)
-		const freezeObj = Object.freeze(env || {})
+		envPath = envPath || path.resolve(process.cwd(), 'config/env')
+		let env = require(envPath) || {}
+		Object.assign(env, env[application['NODE_ENV']])
+		const freezeObj = Object.freeze(env)
 		Object.defineProperties(application, { '_env': { "get": () => { return freezeObj } } })
 	}
 }
